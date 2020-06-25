186597e8fa537edce4dd6c710708a595fb658761
IsadoraMartinez <isadoramartinez123@gmail.com>
2020-06-24 21:29:22 -0400

    Create reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+const songsReducer = () => {
+	return [
+	{title: 'Hit Me Baby One More Time', duration: "2:35"},
+	{title: 'Smells Like Teen Spirit', duration: "3:02"},
+	{title: 'Macarena', duration: "4:08"},
+	{title: 'No Scrubs', duration: "3:35"}
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	
+	if(action.type === 'SONG_SELECTED'){
+		return action.payload;
+	}
+
+	return selectedSong;
+};
